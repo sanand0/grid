ebcbf5ed383564085ec78489214ce48f2b52f010
Akash A <akash1810@gmail.com>
2015-10-15 11:49:16 +0100

    DRYing template and adding formatting.

--- kahuna/public/js/components/gr-image-usage/gr-image-usage.js
+++ kahuna/public/js/components/gr-image-usage/gr-image-usage.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
+import moment from 'moment';
+
 import template from './gr-image-usage.html!text';
+import usageTemplate from './gr-image-usage-list.html!text';
 import './gr-image-usage.css!';
 
 export let module = angular.module('gr.imageUsage', []);
@@ -10,10 +13,20 @@ module.controller('grImageUsageCtrl', ['mediaUsage', function (mediaUsage) {
     mediaUsage.getUsage(ctrl.image).then(data => {
         ctrl.usage = data;
     });
+
+    ctrl.usageTypeToName = (usageType) => {
+        switch (usageType) {
+            case 'pending':
+                return 'Pending publication';
+            case 'published':
+                return 'Published';
+            default:
+                return usageType;
+        }
+    };
 }]);
 
 module.directive('grImageUsage', [function() {
-
     return {
         restrict: 'E',
         template: template,
@@ -25,5 +38,27 @@ module.directive('grImageUsage', [function() {
             image: '=grImage'
         }
     };
+}]);
 
+module.controller('grImageUsageListCtrl', [function () {
+    let ctrl = this;
+
+    ctrl.formatTimestamp = (timestamp) => {
+        return moment(timestamp).fromNow();
+    }
+}]);
+
+
+module.directive('grImageUsageList', [function () {
+    return {
+        restrict: 'E',
+        template: usageTemplate,
+        controller: 'grImageUsageListCtrl',
+        controllerAs: 'ctrl',
+        bindToController: true,
+        scope: {
+            type: '=',
+            usages: '='
+        }
+    }
 }]);
